a0e9f4fac929923989835ea5c0c3f9540b785ed8
kirpit <kirpit@gmail.com>
2013-04-19 14:16:17 +0300

    insecure static server support

--- sampleapp/urls.py
+++ sampleapp/urls.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python2.7
 from django.conf import settings
 from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
+from django.contrib.staticfiles.views import serve
 from django.contrib import admin
 from sampleapp.home.views import HomeView
 
@@ -11,9 +11,12 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', HomeView.as_view(), name='home'),
 
+    # static
+    url(r'^%s(?P<path>.*)$' % settings.STATIC_URL.lstrip('/'), serve,
+        {'show_indexes': True, 'insecure': False}),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin_tools/', include('admin_tools.urls')),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)

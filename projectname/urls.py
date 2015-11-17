29a3bace680f78020186d9f840ccfa66be360461
Adrian Lopez <adrianlzt@gmail.com>
2015-11-17 16:51:23 +0100

    Cambiado {{ project_name }} por projectname y puesta una key dummy con XXXXX...

--- projectname/urls.py
+++ projectname/urls.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python2.7
-"""{{ project_name }} URL Configuration
+"""projectname URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
--- projectname/wsgi.py
+++ projectname/wsgi.py
@@ -16,7 +16,7 @@ framework.
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ['DJANGO_SETTINGS_MODULE'] = '{{ project_name }}.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'projectname.settings'
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
